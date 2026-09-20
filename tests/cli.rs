rsn491/agctxlint

Send requested usage to stdout
ctxlint --help wrote its usage to stderr and exited 0, so `ctxlint --help |
less` showed nothing and `ctxlint --help > usage.txt` wrote an empty file.

Usage the user asked for now goes to stdout. Usage printed because a flag was
wrong is diagnostic output and stays on stderr with the error, leaving stdout
clean for a caller parsing it -- the test asserts both directions.

Co-Authored-By: Claude Opus 5 <noreply@anthropic.com>
Claude-Session: https://claude.ai/code/session_01MwxVMRQrCeuBi2F3XB2ZYh

tests/cli.rs
@@ -518,11 +518,25 @@ fn version_and_list_rules() {
 }
 
 #[test]
-fn help_exits_zero() {
-    let (code, _, stderr) = run_args(&["-h"]);
-    assert_eq!(code, EXIT_OK);
+fn help_goes_to_stdout_and_exits_zero() {
+    for flag in ["-h", "--help"] {
+        let (code, stdout, stderr) = run_args(&[flag]);
+        assert_eq!(code, EXIT_OK, "{flag}");
+        assert!(
+            stdout.contains("ctxlint lints agent instruction files"),
+            "{flag}: {stdout}"
+        );
+        assert!(stderr.is_empty(), "{flag}: {stderr}");
+    }
+
+    // Usage printed because a flag was wrong is diagnostic output, so it
+    // stays on stderr alongside the error and leaves stdout clean.
+    let (code, stdout, stderr) = run_args(&["--nope"]);
+    assert_eq!(code, EXIT_USAGE);
+    assert!(stdout.is_empty(), "{stdout}");
     assert!(
-        stderr.contains("ctxlint lints agent instruction files"),
+        stderr.contains("flag provided but not defined")
+            && stderr.contains("ctxlint lints agent instruction files"),
         "{stderr}"
     );
 }

rsn491/lintmatter

tests/cli.rs
@@ -543,11 +543,25 @@ fn version_and_list_rules() {
 }
 
 #[test]
-fn help_exits_zero() {
-    let (code, _, stderr) = run_args(&["-h"]);
-    assert_eq!(code, EXIT_OK);
+fn help_goes_to_stdout_and_exits_zero() {
+    for flag in ["-h", "--help"] {
+        let (code, stdout, stderr) = run_args(&[flag]);
+        assert_eq!(code, EXIT_OK, "{flag}");
+        assert!(
+            stdout.contains("lintmatter lints agent instruction files"),
+            "{flag}: {stdout}"
+        );
+        assert!(stderr.is_empty(), "{flag}: {stderr}");
+    }
+
+    // Usage printed because a flag was wrong is diagnostic output, so it
+    // stays on stderr alongside the error and leaves stdout clean.
+    let (code, stdout, stderr) = run_args(&["--nope"]);
+    assert_eq!(code, EXIT_USAGE);
+    assert!(stdout.is_empty(), "{stdout}");
     assert!(
-        stderr.contains("lintmatter lints agent instruction files"),
+        stderr.contains("flag provided but not defined")
+            && stderr.contains("lintmatter lints agent instruction files"),
         "{stderr}"
     );
 }